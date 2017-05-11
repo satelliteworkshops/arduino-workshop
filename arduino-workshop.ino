299592c7a32b1d7d621a498cc893ad9f2b6f4119
Jamie Strusz <allthedoll@github.com>
2017-05-10 20:49:51 -0700

    Add initial hello world

---
+++ arduino-workshop.ino
@@ -0,0 +1,27 @@
+#include <TFT.h>  // Arduino LCD library
+#include <SPI.h>
+
+// pin definition for LCD
+#define cs   10
+#define dc   9
+#define rst  8
+
+TFT TFTscreen = TFT(cs, dc, rst);
+
+
+void setup() {
+  // put your setup code here, to run once:
+  // initialize the display
+  TFTscreen.begin();
+  // clear the screen with a black background
+  TFTscreen.background(0, 0, 0);
+  // set the font color to white
+  TFTscreen.stroke(255, 255, 255);
+  // print test text
+  TFTscreen.text("Hello world", 0, 20);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
