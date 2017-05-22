9815242b2ab27d17f555903900178aaf0af1489b
Jamie Strusz <allthedoll@github.com>
2017-05-22 21:31:01 +0100

    Update libraries for display hardware change

--- WeatherThing.ino
+++ WeatherThing.ino
@@ -1,5 +1,7 @@
-#include "lcd.h"
+#include "display.h"
 #include "sensor.h"
+#include <LiquidCrystal.h>  // Arduino LCD library
+#include <Arduino.h>  // enables use of byte pics
 
 void setup() {
   LCD::setup();
